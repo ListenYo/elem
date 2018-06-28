550df4be4c1d303b2bf0761d690dd5a4d1a3246f
bilibili22 <38396197+bilibili22@users.noreply.github.com>
2018-06-28 22:07:40 +0800

    Update hungry.js

--- hungry.js
+++ hungry.js
@@ -15,17 +15,17 @@ class Hungry extends React.Component{
                     <li>
                         <p>品质套餐</p>
                         <span>搭配齐全吃得好</span>
-                        <img src="//fuss10.elemecdn.com/d/d4/16ff085900d62b8d60fa7e9c6b65dpng.png?imageMogr/format/webp/thumbnail/!240x160r/gravity/Center/crop/240x160/" alt="">
+                        <img src="//fuss10.elemecdn.com/d/d4/16ff085900d62b8d60fa7e9c6b65dpng.png?imageMogr/format/webp/thumbnail/!240x160r/gravity/Center/crop/240x160/"/>
                     </li>
                     <li>
                         <p>品质联盟</p>
                         <span>品质升级，消费亲民</span>
-                        <img src="//fuss10.elemecdn.com/b/e1/0fa0ed514c093a7138b0b9a50d61fpng.png?imageMogr/format/webp/thumbnail/!240x160r/gravity/Center/crop/240x160/" alt="">
+                        <img src="//fuss10.elemecdn.com/b/e1/0fa0ed514c093a7138b0b9a50d61fpng.png?imageMogr/format/webp/thumbnail/!240x160r/gravity/Center/crop/240x160/"/>
                     </li>
                     <li>
                         <p>限量抢购</p>
                         <span>超值美味  9.9元起</span>
-                        <img src="//fuss10.elemecdn.com/d/d4/16ff085900d62b8d60fa7e9c6b65dpng.png?imageMogr/format/webp/thumbnail/!240x160r/gravity/Center/crop/240x160/" alt="">
+                        <img src="//fuss10.elemecdn.com/d/d4/16ff085900d62b8d60fa7e9c6b65dpng.png?imageMogr/format/webp/thumbnail/!240x160r/gravity/Center/crop/240x160/"/>
                     </li>
                 </ul>
             </div>

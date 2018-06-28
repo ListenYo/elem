2000a53b4a3d9f596d0fa073170a7213bef23fbd
吾爱丶本倾城 <1564188435@qq.com>
2018-06-28 20:47:17 +0800

    魏航banner

---
+++ banner.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Administrator on 2018-06-28.
+ */
+import React from 'react'
+import '../stylesheets/banner.css'
+class Banner extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+                <ul className='oul'>
+                	<li><img src="https://fuss10.elemecdn.com/7/d8/a867c870b22bc74c87c348b75528djpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/"/>
+                	<p>美食</p>
+                	</li>
+                	<li><img src="https://fuss10.elemecdn.com/e/89/185f7259ebda19e16123884a60ef2jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/"/>
+                	<p>晚餐</p>
+                	</li>
+                	<li><img src="https://fuss10.elemecdn.com/c/3c/0184f5b3fa72f295fc01864734218jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/"/>
+                	<p>商城便利</p>
+                	</li>
+                	<li><img src="https://fuss10.elemecdn.com/c/db/d20d49e5029281b9b73db1c5ec6f9jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/"/>
+                	<p>果鲜生蔬</p>
+                	</li>
+                	<li><img src="https://fuss10.elemecdn.com/e/8a/1a58f9145075322665b12552da1aejpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/
+"/>
+                	<p>新店特惠</p>
+                	</li>
+                	<li><img src="https://fuss10.elemecdn.com/7/0a/af108e256ebc9f02db599592ae655jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/"/>
+                	<p>医药健康</p>
+                	</li>
+                	<li><img src="https://fuss10.elemecdn.com/a/7b/b02bd836411c016935d258b300cfejpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/"/>
+                	<p>大牌5折</p>
+                	</li>
+                	<li><img src="https://fuss10.elemecdn.com/3/01/c888acb2c8ba9e0c813f36ec9e90ajpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/"/>
+                	<p>浪漫鲜花</p>
+                	</li>
+                	<li><img src="https://fuss10.elemecdn.com/e/c7/b7ba9547aa700bd20d0420e1794a8jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/"/>
+                	<p>麻辣烫</p>
+                	</li>
+                	<li><img src="https://fuss10.elemecdn.com/a/8a/ec21096d528b7cfd23cdd894f01c6jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/"/>
+                	<p>地方菜系</p>
+                	</li>
+                	
+                </ul>
+            </div>
+        )
+    }
+}
+export default Banner;
